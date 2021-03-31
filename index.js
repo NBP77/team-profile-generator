a1d5959af1569b3e80c847264826ea9ac99aaf02
NBP77 <nbp77@outlook.com>
2021-03-30 22:49:54 -0400

    Added engineer and intern

--- index.js
+++ index.js
@@ -15,41 +15,107 @@ let finalTeamArray = []
 function addManager() {
     inquirer.prompt([
         {
-        message: "----What is your managers name?----",
+        message: "----What is your Managers name?----",
         name: "managerName"
     },
     {
         type: "number",
-        message: "----What is your managers id number?----",
+        message: "----What is your Managers id number?----",
         name: "idNumber"
     },
     {
-        message: "----What is your managers email address?----",
+        message: "----What is your Managers email address?----",
         name: "email"
     },
     {   
         type: "number",
-        message: "----What is your managers office number?----",
+        message: "----What is your Managers office number?----",
         name: "officeNumber"
     },
 
 ])
 
     .then(function (data) {
-        const name = data.name
+        const managerName = data.managerName
         const idNumber = data.idNumber
         const email = data.email
         const officeNumber = data.officeNumber
-        const teamMember = new Manager(name, idNumber, email, officeNumber)
+        const teamMember = new Manager(managerName, idNumber, email, officeNumber)
         finalTeamArray.push(teamMember)
         addTeamMembers();
     });
-}
+};
 
 // add engineer
 
+function addEngineer() {
+    inquirer.prompt([
+        {
+        message: "----What is your Engineers name?----",
+        name: "engineersName"
+    },
+    {
+        type: "number",
+        message: "----What is your Engineers id number?----",
+        name: "idNumber"
+    },
+    {
+        message: "----What is your Engineers email address?----",
+        name: "email"
+    },
+    {   
+        type: "number",
+        message: "----What is your Engineers GitHub Username?----",
+        name: "githubUsername"
+    },
 
+])
 
+    .then(function (data) {
+        const engineersName = data.engineersName
+        const idNumber = data.idNumber
+        const email = data.email
+        const githubUsername = data.githubUsername
+        const teamMember = new Engineer(engineersName, idNumber, email, githubUsername)
+        finalTeamArray.push(teamMember)
+        addTeamMembers();
+    });
+};
 
 // add intern
+
+function addIntern() {
+    inquirer.prompt([
+        {
+        message: "----What is your Interns name?----",
+        name: "internsName"
+    },
+    {
+        type: "number",
+        message: "----What is your Interns id number?----",
+        name: "idNumber"
+    },
+    {
+        message: "----What is your Interns email address?----",
+        name: "email"
+    },
+    {   
+        type: "number",
+        message: "----What school did your Intern go to?----",
+        name: "internsSchool"
+    },
+
+])
+
+    .then(function (data) {
+        const internsName = data.internsName
+        const idNumber = data.idNumber
+        const email = data.email
+        const internsSchool = data.internsSchool
+        const teamMember = new Engineer(internsName, idNumber, email, internsSchool)
+        finalTeamArray.push(teamMember)
+        addTeamMembers();
+    });
+};
+
 // Put team together in html using template literals 
